68a95bee71e0fead105130d540c9ed367c5b3b2c
sabbakar <sadeeqsas14@gmail.com>
2023-09-27 12:09:12 +0100

    Some little Puppet Styling Adjustment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -5,9 +5,9 @@
 # File group is www-data
 # File contains I love Puppet
 file { '/tmp/school':
-  ensure => present,
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
